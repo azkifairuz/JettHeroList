40c65bfdc7a73683d5490431f854cbf2cac0d434
azkifairuz <azkiajmal@gmail.com>
2023-11-11 14:16:47 +0700

    create data class hero

---
+++ app/src/main/java/com/azkifairuz/jetherolist/model/Hero.kt
@@ -0,0 +1,7 @@
+package com.azkifairuz.jetherolist.model
+
+data class Hero(
+    val id: String,
+    val name: String,
+    val photoUrl: String
+)
---
+++ app/src/main/java/com/azkifairuz/jetherolist/model/HeroesData.kt
@@ -0,0 +1,4 @@
+package com.azkifairuz.jetherolist.model
+
+object HeroesData {
+}
